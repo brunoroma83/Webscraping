32d48df5d9d60646596564450ea41c233b70c90a
Bruno Roma <bruuno@gmail.com>
2024-03-30 14:04:10 -0300

    incluindo IA Gemini API

--- gemini_api.py
+++ gemini_api.py
@@ -1,1 +1,5 @@
 # integrar o gemini no webscraping, inicialmente para encontrar algumas informações da extração do html, como por exemplo o nome da empresa.
+
+from dotenv import dotenv_values
+
+envs = dotenv_values('.env')
--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import os
+from dotenv import dotenv_values
+
+envs = dotenv_values('.env')
 
 df = pd.read_json('reg_anvisa_alerta.json')
 #print(df.info)
 print(df['Alerta'].max())
 print(df.value_counts('Número de registro ANVISA'))
+print(f'gemini = {envs["GEMINI_API_KEY"]}')
